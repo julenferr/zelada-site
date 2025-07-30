36582439f72ebe28604bc2b015a260e48e11b2cf
Seba <seba@deportv.ar>
2025-07-29 21:19:49 -0300

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -10,9 +10,11 @@ export default function Footer() {
     			<li className="mb-7 mt-2">
     			   <span className="mb-2" >CONTACTO</span>
     			   <a className="block ml-9" href="#">info@zeladaepstein.com.ar</a>
-                   <a className="inline ml-9" href="+5491154952629">+54 9 11 5495-2629</a>&nbsp;&nbsp;/&nbsp;
-                   <a className="md:inline block md:ml-0 ml-9" href="+5491132463210">+54 9 11 3246-3210</a>
-    			</li>
+                   <div className="ml-9">
+                       <a className="inline" href="+5491154952629">+54 9 11 5495-2629</a>&nbsp;&nbsp;/&nbsp;
+                       <a className="md:inline block" href="+5491132463210">+54 9 11 3246-3210</a>
+    			   </div>
+                </li>
     			<li>
     			   <span className="mb-2">DIRECCIÓN</span>
     			   <a className="ml-9 block" href="#">Av. Gral. Benjamín Victorica 3101, C1427DGD Ciudad Autónoma de Buenos Aires</a>
