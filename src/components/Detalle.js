c9ff58ab0a6a87c458d9d45d305a4522e2dd1050
Seba <seba@deportv.ar>
2025-07-30 20:50:07 -0300

    detalle

--- src/components/Detalle.js
+++ src/components/Detalle.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Image from "next/image";
 import Lightbox from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
