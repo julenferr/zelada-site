fe9931118a7ad8356377742cbe88b54d33bf272f
Seba <seba@deportv.ar>
2025-08-05 09:33:35 -0300

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,18 +6,18 @@ export default function Footer() {
     	<img data-scroll data-scroll-repeat="repeat" data-scroll-delay="5" data-scroll-class="is-inview" className="in-view pl-5 pr-5 col-span-12" src="/logo-blanco.svg" alt="Logo estudio Zelada Epstein" />
     	<div className="pl-5 pr-5 col-span-12 grid grid-cols-4 md:grid-cols-12">
     		<p className="col-start-1 md:col-start-2 col-span-12 md:col-span-3 font-light md:mb-0 mb-10">Somos un estudio de diseño con fuerte impronta en la <i>ilustración</i>.</p>
-    		<ul className="col-start-1 md:col-start-8 col-span-12 md:col-span-3">
+    		<ul className="col-start-1 md:col-start-8 col-span-12 md:col-span-4">
     			<li className="mb-7 mt-2">
     			   <span className="mb-2" >CONTACTO</span>
     			   <a className="block ml-9" href="#">info@zeladaepstein.com.ar</a>
                    <div className="ml-9">
                        <a className="inline" href="+5491154952629">+54 9 11 5495-2629</a>&nbsp;&nbsp;/&nbsp;
-                       <a className="md:inline block" href="+5491132463210">+54 9 11 3246-3210</a>
+                       <a className="lg:inline block" href="+5491132463210">+54 9 11 3246-3210</a>
     			   </div>
                 </li>
     			<li>
     			   <span className="mb-2">DIRECCIÓN</span>
-    			   <a className="ml-9 block" href="#">Av. Gral. Benjamín Victorica 3101, C1427DGD Ciudad Autónoma de Buenos Aires</a>
+    			   <a className="ml-9 block" href="#">Av. Gral. Benjamín Victorica 3101,<br /> C1427DGD Ciudad Autónoma de Buenos Aires</a>
     			</li>
     		</ul>
     	</div>
